29ab38f190408d662149f8567d97b14c0f81a397
Rishav Bharti <brishabh619@gmail.com>
2021-01-07 13:22:51 +0530

    feat: added quantity increase and decrease functionality

--- src/CartItem.js
+++ src/CartItem.js
@@ -6,12 +6,50 @@ class CartItem extends React.Component{
         this.state = {
             title: 'Mobile Phone',
             price: 8999,
-            qty: 2,
+            qty: 1,
             image: ''
         }
     }
-    IncreaseQty =()=> {
-        console.log(this.state);
+
+
+// SET STATE FUNTION 
+        
+
+        // setState form 1 : object method
+
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // })
+
+        // setState form 2 : fuction method
+
+        // this.setState((prevState)=>{
+        //     return{
+        //         qty: prevState.qty += 1
+        //     }
+        // })
+
+
+
+
+    increaseQty =()=> {
+        this.setState((prevState)=>{
+            return{
+                qty: prevState.qty += 1
+            }
+        })
+    }
+    
+    decreaseQty=()=>{
+        const {qty} = this.state;
+        if(qty===0){
+            return;
+        }
+        this.setState((prevState)=>{
+            return{
+                qty: prevState.qty-=1
+            }
+        })
     }
     render(){
         const { title, price, qty, image } = this.state;
@@ -25,12 +63,21 @@ class CartItem extends React.Component{
                     <div style={{color: '#777'}}>Rs {price}</div>
                     <div style={{color: '#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
-                        <img src="https://www.flaticon.com/svg/static/icons/svg/992/992651.svg" 
+                        <img 
+                        src="https://www.flaticon.com/svg/static/icons/svg/992/992651.svg" 
                         alt="increase" 
                         className="action-icons"
-                        onClick={this.IncreaseQty}/>
-                        <img src="https://www.flaticon.com/svg/static/icons/svg/992/992683.svg" alt="decrease" className="action-icons"/>
-                        <img src="https://www.flaticon.com/svg/static/icons/svg/1345/1345874.svg" alt="delete" className="action-icons"/>
+                        onClick={this.increaseQty}/>
+                        <img 
+                        src="https://www.flaticon.com/svg/static/icons/svg/992/992683.svg" 
+                        alt="decrease" 
+                        className="action-icons"
+                        onClick={this.decreaseQty}
+                        />
+                        <img 
+                        src="https://www.flaticon.com/svg/static/icons/svg/1345/1345874.svg" 
+                        alt="delete" 
+                        className="action-icons"/>
                     </div>
                 </div>
             </div>
